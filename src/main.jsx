71e783a2c6a28d826bda47675da2e026dcf9bc96
Aayush-Timalsina <aayushtimalsina@example.com>
2024-08-21 21:20:23 +0545

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import AppContextProvider from './context/appContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
